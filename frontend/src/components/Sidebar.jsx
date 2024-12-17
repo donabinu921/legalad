2120f4c58c56a6fc1ead6f252323552db1fa52ae
donabinu921 <elizabethdona921@gmail.com>
2024-12-17 22:00:12 +0530

    logout button, sidebar adjustments

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,41 +1,51 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { FaHome, FaFileAlt, FaFileSignature, FaRobot } from 'react-icons/fa';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FaHome, FaFileAlt, FaFileSignature, FaSignOutAlt, FaChevronLeft, FaChevronRight} from 'react-icons/fa';
 import '../styles/Sidebar.css';
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate()
 
   const handleToggle = () => {
     setIsCollapsed(!isCollapsed);
     document.querySelector('.content').classList.toggle('collapsed', !isCollapsed);
   };
 
+  const handleLogout = () => {
+    navigate('/document-drafter');
+  };
+
   return (
     <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
       <ul>
         <li className={`tab ${location.pathname === '/home' ? 'active' : ''}`}>
-          <FaHome className='icon' />
-          {!isCollapsed && <Link to="/home">Home</Link>}
+          <Link to="/home">
+            <FaHome className='icon' />
+            {!isCollapsed && <span>Home</span>}
+          </Link>
         </li>
         <li className={`tab ${location.pathname.startsWith('/document-drafter') ? 'active' : ''}`}>
-          <FaFileAlt className='icon' />
-          {!isCollapsed && <Link to="/document-drafter">Document Drafter</Link>}
+          <Link to="/document-drafter">
+            <FaFileAlt className='icon' />
+            {!isCollapsed && <span>Document Drafter</span>}
+          </Link>
         </li>
         <li className={`tab ${location.pathname.startsWith('/document-analyser') ? 'active' : ''}`}>
-          <FaFileSignature className='icon' />
-          {!isCollapsed && <Link to="/document-analyser">Document Analyser</Link>}
-        </li>
-        {/* <li className={`tab ${location.pathname.startsWith('/legal-chatbot') ? 'active' : ''}`}>
-          <FaRobot className='icon' />
-          {!isCollapsed && <Link to="/legal-chatbot">Legal Chatbot</Link>}
-        </li> */}
+          <Link to="/document-analyser">
+            <FaFileSignature className='icon' />
+            {!isCollapsed && <span>Document Analyser</span>}
+          </Link>
+        </li>      
       </ul>
-
-      {/* Toggle button */}
-      <div className='toggle-button' onClick={handleToggle}>
-        {isCollapsed ? '>' : '<'}
+      <div className='logtoggle'>
+      <button className="logout-button" onClick={handleLogout}>
+            <FaSignOutAlt className='icon' />
+      </button>
+      <button className="toggle-button" onClick={handleToggle}>
+        {isCollapsed ? <FaChevronRight/> : <FaChevronLeft/>}
+      </button>
       </div>
     </div>
   );
