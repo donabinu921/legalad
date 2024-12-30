d5a10ca69e0e68c5abcb27a4f0f46b6f2ddb6e8e
AmithSunil <crispyfingers99@gmail.com>
2024-12-30 19:55:19 +0530

    fixed merge

--- frontend/src/components/Will.jsx
+++ frontend/src/components/Will.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const genAI = new GoogleGenerativeAI(`${process.env.REACT_APP_GEMINI_API_KEY}`);
 
